846fb871bbd59acf313cb1bf8ffd09b160e93ffb
Fidelkyalo <fidelkm16@gmail.com>
2026-04-12 01:06:21 +0300

    Optimize: 2-column mobile projects with glassmorphism overlays

--- js/main.js
+++ js/main.js
@@ -98,7 +98,8 @@
         ],
         responsive: {
             0: {
-                items: 1
+                items: 2,
+                margin: 15
             },
             768: {
                 items: 2
